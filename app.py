cb2bc3dc784b95d04e06aba1a00d650fde049d1d
ParkMini <parkteammini@gmail.com>
2023-03-09 10:27:19 +0900

    Webhook send Function

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
 # 
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import requests
-import socket
 import json
+import time
+import datetime as dt
 
 webhookurl = ""
+
+# Source Code
+# do not Edit This
+runTime = str(dt.datetime.now())
 ip = ""
+lastGetTime = ""
 
-res = requests.get("http://ip-api.com/json/?fields=query")
-res = json.loads(str(res.text))
-ip = res["query"]
+def getip():
+    global ip
+    global lastGetTime
+    res = requests.get("http://ip-api.com/json/?fields=query")
+    res = json.loads(str(res.text))
+    ip = res["query"]
+    lastGetTime = str(dt.datetime.now())
 
+# [ Status Code ]
+# 1 : Program is started
+# 2 : IP is Not Changed
+# 3 : IP is Changed
+# 4 : Failed get IP
 
-# webhook = DiscordWebhook(url=webhookurl)
-# embed = DiscordEmbed(title='DiscordNetStatus', description='Lorem ipsum dolor sit', color=0x03b2f8)
-# embed.set_footer(text='Embed Footer Text', icon_url='URL of icon')
-# embed.add_embed_field(name='Field 1', value='Lorem ipsum')
-# embed.add_embed_field(name='Field 2', value='dolor sit')
-# webhook.add_embed(embed)
-# response = webhook.execute()
+def send_webhook(statusCode):
+    webhook = DiscordWebhook(url=webhookurl)
+    if (statusCode == 1 or statusCode == 2):
+        embed = DiscordEmbed(title='DiscordNetStatus', description='Check your Network Status!', color=0x03b2f8)
+    elif (statusCode == 3):
+        embed = DiscordEmbed(title='DiscordNetStatus', description='IP Address is changed!', color=0xdddd00)
+    elif (statusCode == 4):
+        embed = DiscordEmbed(title='DiscordNetStatus', description='Failed to Get IP!', color=0xdd3322)
+
+    embed.set_footer(text='Made by. ParkMini')
+    embed.add_embed_field(name='IP Address', value=ip)
+    embed.add_embed_field(name='Run Time', value=runTime)
+    embed.add_embed_field(name='Last Check Time', value=lastGetTime)
+    webhook.add_embed(embed)
+    response = webhook.execute()
+    print(response)
